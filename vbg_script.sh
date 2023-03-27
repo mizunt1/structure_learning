fd4c5b7b44e49b257d5ebb181148fa028b900f97
mizunt1 <mizunt@gmail.com>
2023-03-27 18:53:50 -0400

    now dont need to uninstall torch for script to work

--- vbg_script.sh
+++ vbg_script.sh
@@ -8,9 +8,9 @@
 WANDB_API_KEY=$17a113b4804951bde9c66b2002fe378c0209fb64
 WANDB_ENTITY=$mizunt
 module load anaconda/3
+module load cudatoolkit/11.1
 conda create -n envname python=3.10
 conda activate envname
 conda install jax cuda-nvcc jaxlib==0.4.4=cuda112* cudatoolkit -c conda-forge -c nvidia
 pip install -r requirements.txt
-pip uninstall -y torch
 python main.py vbg --num_iterations 10 
