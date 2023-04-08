e40f60b1549a7ec35f22d736ee8f2ba4cfb6cc51
mizunt1 <mizunt@gmail.com>
2023-04-08 11:54:37 -0400

    added plotting and scripts for wandb

---
+++ plotting/box_plots.py
@@ -0,0 +1,69 @@
+import sys, os
+sys.path.append(os.getcwd())
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import pandas as pd
+
+def plot(base_dir, method_path, method_str, ordered_str,fig_path):
+    method_dict = dict(zip(method_str, method_path))
+    result = "results2"
+    df = pd.read_csv(base_dir + '/' + method_path[0], index_col=0)
+    metrics  = ['nll']
+    #metrics = df.columns.to_list()
+    dfs = {}
+    # for all metrics, put in dataframe with name of method
+    for metric in metrics:
+        data_for_metric = {}
+        for method in ordered_str:
+            stri = method_dict[method]
+            file_path = base_dir + '/' + stri
+            data = pd.read_csv(file_path, index_col=0)[metric]
+            data_for_metric[method] = data.to_list()
+        dfs[metric] = pd.DataFrame.from_dict(data_for_metric, orient="index")
+
+
+    # sns.set(font="Verdana")
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["mathtext.fontset"] = "dejavuserif"
+    font = {'family' : 'serif',
+            'size'   : 12}
+
+    mpl.rc('font', **font)
+    
+    for metric in metrics:
+        
+        plt.figure(figsize=(6,5.7), dpi=100)
+        plt.clf()
+        print(metric)
+        if metric == 'nll':
+            plot_is = sns.boxplot(data=(dfs[metric].drop('MH').T), showfliers = False)
+        else:
+            plot_is = sns.boxplot(data=(dfs[metric].T))
+        plt.xticks(rotation=20)
+        data = dfs[metric].T
+        quantiles = data.quantile([0.5, 0.25, 0.75])
+        metric = metric.replace(" ", "_")
+        quantiles.to_csv(fig_path + '/' + metric + ".csv")
+        fig = plot_is.get_figure()
+        fig.savefig(fig_path +'/' + metric + ".png")
+
+if __name__ == '__main__':
+    nodes_5 = True
+    if nodes_5:
+        base_dir = '/network/scratch/m/mizu.nishikawa-toomey/sl/n5'
+        methods = ['ges_arxiv2_n5', 'bcd_arxiv2', 'dibs_plus_arxiv2_n5', 'pc_arxiv2_n5', 'dibs_arxiv2_n5', 'gibs_arxiv2_n5',  'mh_arxiv2_n5','vbg_arxiv2_w_0.5']
+    
+        method_path = [method + '.csv' for method in methods]
+        fig_path = 'figs_arxiv2_final_n5'
+    else:
+        base_dir = '/network/scratch/m/mizu.nishikawa-toomey/sl/n20'
+        method_path = ['ges_arxiv2_n20.csv', 'bcd_arxiv_n20.csv', 'dibs_plus_arxiv2_n20.csv', 'pc_arxiv2_n20.csv', 'dibs_arxiv2_n20.csv', 'gibbs_arxiv2_n20.csv', 'mh_arxiv2_n20.csv','vbg_arxiv2_w_0.5_n20.csv']
+        fig_path = 'figs_arxiv2_n5/'
+
+    method_str = ['BS GES', 'bcd nets', 'DiBS +', 'BS PC', 'DiBS', 'Gibbs', 'MH', 'VBG']
+    ordered_str = ['VBG', 'DiBS', 'DiBS +', 'bcd nets',
+                   'BS GES',  'BS PC',  'Gibbs', 'MH']
+    #, 'MH_burn', 'MH_theta']
+
+    plot(base_dir, method_path, method_str, ordered_str, fig_path)
---
+++ plotting/scrape_wandb.py
@@ -0,0 +1,65 @@
+import wandb
+import pandas as pd
+from collections import defaultdict
+import json
+# this script scrapes a project (containing one inference method), 
+# loops through all the seeds and saves all the metrics for each seed for that one method.
+# means and sd for one metric not calculated, this is done automatically by the plotting script
+api = wandb.Api(timeout=19)
+entity = 'mizunt'
+def get_runs(method):
+    return api.runs(entity + '/' + method,
+        filters={'$and': [{
+            'state': {'$eq': 'finished'},
+        }]}
+    )
+
+def combine_runs(methods, base_dir, nodes_5):
+    for method in methods:
+        project = method
+        runs = get_runs(method)
+        edge_mse = defaultdict()
+        path_mse = defaultdict()
+        markov_mse = defaultdict()
+        shd = defaultdict()
+        auroc = defaultdict()
+        nll = defaultdict()
+        mse = defaultdict()
+        for run_ in runs:
+            summary = run_.summary
+            seed = str(json.loads(run_.json_config)['seed']['value'])
+            if nodes_5:
+                edge_mse[str(seed)] = summary['edge mse']
+                path_mse[str(seed)] = summary['path mse']
+                markov_mse[str(seed)] = summary['markov mse']
+            shd[str(seed)] = summary['metrics/shd/mean']
+            auroc[str(seed)] = summary['metrics/thresholds']['roc_auc']
+            nll[str(seed)] = summary['negative log like']
+            mse[str(seed)] = summary['mse of mean']
+        if nodes_5:
+            df = pd.DataFrame(
+                list(zip(edge_mse.values(),
+                         path_mse.values(),
+                         markov_mse.values(), shd.values(),
+                         auroc.values(), nll.values(),
+                         mse.values())),
+                columns=['edge mse', 'path mse', 'markov mse', 'shd', 'auroc', 'nll', 'mse theta'])
+        else:
+            df = pd.DataFrame(
+                list(zip(shd.values(),
+                         auroc.values(), nll.values(),
+                         mse.values())),
+                columns=['shd', 'auroc', 'nll', 'mse theta'])
+        df.to_csv(base_dir +'/' + project + '.csv')
+        
+
+if __name__ == '__main__':
+    nodes_5 = False
+    if nodes_5:
+        base_dir = '/network/scratch/m/mizu.nishikawa-toomey/sl/n20'
+        methods = ['ges_arxiv2_n20', 'bcd_arxiv_n20', 'vbg_arxiv2_w_0.5_n20', 'dibs_plus_arxiv2_n20', 'pc_arxiv2_n20', 'dibs_arxiv2_n20', 'gibbs_arxiv2_n20', 'mh_arxiv2_n20']
+    else:
+        methods = ['ges_arxiv2_n5', 'bcd_arxiv2', 'vbg_arxiv3_n5', 'dibs_plus_arxiv2_n5', 'pc_arxiv2_n5', 'dibs_arxiv2_n5', 'gibs_arxiv2_n5',  'mh_arxiv2_n5', 'vbg_arxiv2_w_0.5']
+        methods = ['mh_arxiv2_n5_burn', 'mh_arxiv2_n20_theta']
+
+    combine_runs(methods, base_dir, nodes_5)
